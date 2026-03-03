7b94bb7f0197cc40ccfbb6b8bc33427e40f01580
sumayya6652 <105160520+sumayya6652@users.noreply.github.com>
2026-03-03 21:14:59 +1100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -12,9 +12,11 @@ export default function Home() {
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
-        <AchievementsSection />
+        
         <AboutSection />
+      
         <ProjectsSection />
+        <AchievementsSection />
         <EmailSection />
       </div>
       <Footer />
